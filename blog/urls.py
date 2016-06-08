9c6154beaade5d80fdfbf340a99b558378dec3a6
Dantemun <dantemode@naver.com>
2016-06-08 15:48:01 +0900

    Shop_detail 페이지 구현

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'blog.views.index', name='index'),
+    url(r'^(?P<pk>\d+)/$', 'blog.views.shop_detail', name='shop_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Shop
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def shop_detail(request, pk):
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, 'blog/shop_detail.html', {
+        'shop' : shop,
+        })
